abe67c56bbe8feba8ba17fc3839636a2083fc7a8
ferutizi <fer.utizi@gmail.com>
2022-07-13 23:07:19 -0300

    estilo carrito

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -6,8 +6,13 @@ const CartItem = ({ data, deleteOneFromCart, deleteAllFromCart }) => {
 
     return(
         <div className='cart--card'>
-            <h4 className='cart--name'>{name}    ({amount})</h4>
-            <h5 className='cart--price'>${price} X {amount} = ${price * amount}</h5>
+            <div className='text--container'>
+                <div className='titleprice'>
+                    <h4 className='cart--name'>{name}    ({amount})</h4>
+                    <h5 className='cart--price'>${price} X {amount} = ${price * amount}</h5>
+                </div>
+                <img className="cart--img" src={img} alt={name} />
+            </div>
             <button className='btn--delete' onClick={() => deleteOneFromCart(id)}>Eliminar</button>
             <button className='btn--delete__red' onClick={() => deleteAllFromCart(id, true)}>Eliminar Todos</button>
         </div>
